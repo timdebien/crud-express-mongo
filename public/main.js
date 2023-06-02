49d5b6a0de946398d32e855985c9f98b2e14cf5f
timdebien <timmydibs@gmail.com>
2023-06-02 12:01:05 -0600

    finshed crud app

---
+++ public/main.js
@@ -0,0 +1,45 @@
+// const { response } = require("express")
+
+// main.js
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+  // Send PUT Request here
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body:JSON.stringify({
+        name: 'Darth Vader',
+        quote: 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res => {
+    if(res.ok) return res.json()
+  })
+  .then(response => {
+    window.location.reload(true)
+  })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vader'
+        })
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+    })
+    .then(response => {
+      if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No Darth Vader qu   ote to delete'
+      } else {
+        window.location.reload(true)
+      }
+      
+    })
+})
--- server.js
+++ server.js
@@ -1,13 +1,99 @@
+console.log('may the node be with you')
+// use express in server.js by requiring it.
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://timmydibs:divJ8DmRM6iJHUGc@cluster0.bbuxpou.mongodb.net/?retryWrites=true&w=majority'
 
-app.listen(8000, function () {
-    console.log('listening on 8000')
-  })
+
+
+//connect to the mongo database
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+   // tells Express we’re using EJS as the template engine
+    app.set('view engine', 'ejs')
+    
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+   //read
+    app.get('/', (req, res) => {
+      quotesCollection.find().toArray()
+      .then(results => {
+        res.render('index.ejs', {quotes: results})
+        // console.log(results)
+      })
+      .catch(error => console.error(error))
+      
+    })
+    // creat
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+      .then(result => {
+        // console.log(result)
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+    })
+    //update
+    app.put('/quotes', (req, res) => {
+      // console.log(req.body)
+      quotesCollection.findOneAndUpdate(
+        {name: 'Yoda'},
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert:true
+        }
+      )
+      .then(result => {
+        // console.log(result)
+        res.json('Success')
+      })
+      .catch(error => console.error(error))
+    })
+    //delete
+    app.delete('/quotes', (req,res) => {
+      quotesCollection.deleteOne(
+        {name: req.body.name }
+      )
+      .then(result => {
+        if(result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json("Deleted Darth Vader's quote")
+      })
+      .catch(error => console.error(error))
+    })
+
+    //create server browser can connect to localhost:8000
+    app.listen(8000, function () {
+      console.log('listening on 8000')
+    })
+  }
+)
+.catch(error => console.error(error))
+
+
+
+
+
+
+
   
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
+
+  
 
 
 
+
+
+ 
